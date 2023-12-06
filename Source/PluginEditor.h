b05d9bb9fbbc8778c45c02a6574be71922ebfb84
McSpicyWifBacon <chowenyao@gmail.com>
2023-12-06 12:08:31 +0800

    Customized graphics

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -167,12 +167,17 @@ private:
 
 struct LookAndFeel: juce::LookAndFeel_V4
 {
-    virtual void drawRotarySlider (juce::Graphics&,
+    void drawRotarySlider (juce::Graphics&,
                                    int x, int y, int width, int height,
                                    float sliderPosProportional,
                                    float rotaryStartAngle,
                                    float rotaryEndAngle,
                                    juce::Slider&) override;
+    
+    void drawToggleButton (juce::Graphics &g,
+                           juce::ToggleButton & toggleButton,
+                           bool shouldDrawButtonAsHighlighted,
+                           bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -312,6 +317,8 @@ private:
                         analyzerEnabledButtonAttachment;
     
     std::vector<juce::Component*> getComps();
+    
+    LookAndFeel lnf;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
